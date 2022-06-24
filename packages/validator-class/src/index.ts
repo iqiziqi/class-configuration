2b9c9c8e4c4d177fee7632440290daf3454d20f9
iqiziqi <iqiziqi@outlook.com>
2022-06-24 13:40:52 +0800

    [feat] Implement the define of validator

--- packages/validator-class/src/index.ts
+++ packages/validator-class/src/index.ts
@@ -1,10 +1,20 @@
 import { BaseConfig, IValidator } from '@class-config/core';
-import { validate, ValidatorOptions } from 'class-validator';
+import { validate, ValidationError, ValidatorOptions } from 'class-validator';
+
+function getErrors(errors: ValidationError[]): string[] {
+  const messagesList = errors.map((error) => {
+    if (error.children?.length) return getErrors(error.children).map((property) => `${error.property}.${property}`);
+    else return error.constraints ? Object.values(error.constraints) : [];
+  });
+  const messages = new Array<string>().concat(...messagesList);
+  return messages;
+}
 
 export class ClassValidator implements IValidator {
   public constructor(private readonly validatorOptions?: ValidatorOptions) {}
-  public async validate(config: BaseConfig): Promise<boolean> {
-    const error = await validate(config, this.validatorOptions);
-    return !error.length;
+  public async validate(config: BaseConfig): Promise<string[]> {
+    const errors = await validate(config, this.validatorOptions);
+    const messages = getErrors(errors);
+    return messages;
   }
 }
--- packages/validator-class/tests/index.tests.ts
+++ packages/validator-class/tests/index.tests.ts
@@ -1,6 +1,6 @@
 import { expect } from 'expect';
 import { Config, BaseConfig, ConfigField } from '@class-config/core';
-import { IsString, IsPositive } from 'class-validator';
+import { IsString, IsPositive, IsInstance, ValidateNested, IsNumber } from 'class-validator';
 import { ClassValidator } from '../src';
 
 describe('Test validate config class', function () {
@@ -41,6 +41,37 @@ describe('Test validate config class', function () {
       public port!: number;
     }
 
-    expect(DatabaseConfig.init<DatabaseConfig>({ validator: new ClassValidator() })).rejects.toThrow();
+    expect(DatabaseConfig.init<DatabaseConfig>({ validator: new ClassValidator() })).rejects.toThrow(
+      'port must be a positive number',
+    );
+  });
+
+  it('should throw an error validate failed with nested object', async function () {
+    @Config()
+    class DatabaseConfig extends BaseConfig {
+      @ConfigField({
+        parser: () => 'localhost',
+      })
+      @IsNumber()
+      public host!: number;
+      @ConfigField({
+        parser: () => '8080',
+      })
+      @IsPositive()
+      public port!: number;
+    }
+
+    @Config()
+    class Configuration extends BaseConfig {
+      @ConfigField()
+      @IsInstance(DatabaseConfig)
+      @ValidateNested()
+      public database!: DatabaseConfig;
+    }
+
+    expect(Configuration.init<Configuration>({ validator: new ClassValidator() })).rejects.toThrow(
+      `database.host must be a number conforming to the specified constraints
+      database.port must be a positive number`,
+    );
   });
 });
